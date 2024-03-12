001340601a00db572f5dd3ba1b1ec177fe5c1146
Abdo Mohammad <abdomhammad618@gmail.com>
2024-03-12 17:46:53 +0200

    adding dynamic metadata

--- src/pages/our-clients.js
+++ src/pages/our-clients.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import Head from 'next/head'
-const OurClients = () => {
+import axios from '@/utils/axios'
+const OurClients = ({data}) => {
+  console.log(data)
   return (
     <div>
-    <Head><title>Hello</title></Head>
+    <Head><title>{data.data.title}</title></Head>
       OurClients</div>
   )
+}
+export async function getServerSideProps () {
+  try {
+    const response = await axios.get ('/page/home', {
+      headers: {
+        'Accept-Language': 'en',
+      },
+    });
+
+    const data = response.data;
+
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    console.error ('Error fetching data:', error);
+    
+  }
 }
 
 export default OurClients
