9cfa4b662e25cc6bf04be48b84ac6de53a819ee9
Abdo Mohammad <abdomhammad618@gmail.com>
2024-03-12 17:36:13 +0200

    adding title metadata

--- src/pages/our-clients.js
+++ src/pages/our-clients.js
@@ -1,10 +1,9 @@
 import React from 'react'
-
-
+import Head from 'next/head'
 const OurClients = () => {
   return (
     <div>
-    
+    <Head><title>Hello</title></Head>
       OurClients</div>
   )
 }
