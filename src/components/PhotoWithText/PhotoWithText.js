e09292efd3dcb8d6f73d22586ab7eb83fae3f96c
Abdo Mohammad <abdomhammad618@gmail.com>
2024-03-18 10:53:17 +0200

    editing the photo with text design

--- src/components/PhotoWithText/PhotoWithText.js
+++ src/components/PhotoWithText/PhotoWithText.js
@@ -4,17 +4,17 @@ import Image from 'next/image';
 const PhotoWithText = ({textFirst, image, text, subtext}) => {
   return (
     <div className="container">
-      <div className="grid grid-cols-5 gap-x-5 md:gap-x-12 ">
+      <div className="grid grid-cols-5 gap-x-5  ">
         <div
           style={{order: textFirst ? 1 : 2}}
           className={`flex flex-col justify-center  col-span-2 ${textFirst ? 'items-end' : ''}`}
         >
           <div>
 
-            <p className="text-[0.3rem] xs:text-[0.4rem] sm:text-xs md:text-lg lg:text-2xl xl:text-3xl 2xl:text-4xl font-georama font-normal max-w-24 xs:max-w-28 sm:max-w-40  md:max-w-56 lg:max-w-80 xl:max-w-96 ">
+            <p className="text-[0.6rem] !leading-none xs:text-xs sm:text-lg md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-georama font-normal max-w-24 xs:max-w-28 sm:max-w-44  md:max-w-56 lg:max-w-80 xl:max-w-96 2xl:max-w-[30rem] ">
               {text}
             </p>
-            <p className="text-[0.2rem] xs:text-[0.3rem] sm:text-[0.5rem] md:text-xs lg:text-sm xl:text-base  2xl:text-lg font-georama font-normal max-w-24 xs:max-w-28 sm:max-w-40  md:max-w-56 lg:max-w-80  text-primary-red">
+            <p className="text-[0.3rem] xs:text-[0.4rem] sm:text-[0.6rem] md:text-xs lg:text-lg xl:text-xl 2xl:text-2xl font-georama font-normal max-w-24 xs:max-w-28 sm:max-w-44  md:max-w-56 lg:max-w-80  xl:max-w-96 2xl:max-w-[30rem] text-primary-red">
               {subtext}
             </p>
           </div>
