ffeab4c3f252c50996d1e082fada527e1f071c12
AbdoMohamamd <150178021+AbdoMohamamd@users.noreply.github.com>
2024-08-14 14:12:03 +0300

    fixing error

--- src/pages/our-clients.js
+++ src/pages/our-clients.js
@@ -1,38 +1,29 @@
-import React from 'react';
-import Head from 'next/head';
-import axios from '@/utils/axios';
-import CustomHead from '@/components/CustomHead/CustomHead';
+import React from "react";
+import Head from "next/head";
+import axios from "@/utils/axios";
+import Footer from "@/components/Footer/Footer";
 
-
-
-import CustomHead from '@/components/CustomHead/CustomHead';
-import Footer from '@/components/Footer/Footer';
-
-
-const OurClients = ({data}) => {
-  console.log (data);
+const OurClients = ({ data }) => {
+  console.log(data);
   return (
-
-
-
     <div className="md:pt-8">
       <Head>
         <title>our clients</title>
         <link rel="icon" href={data.data.seo_image} />
         <meta name="description" content={data.data.seo_description} />
-      </Head> <NavBar />
+      </Head>{" "}
+      <NavBar />
       <Banner />
-
       OurClients
       <Footer />
     </div>
   );
 };
-export async function getStaticProps () {
+export async function getStaticProps() {
   try {
-    const response = await axios.get ('/page/home', {
+    const response = await axios.get("/page/home", {
       headers: {
-        'Accept-Language': 'en',
+        "Accept-Language": "en",
       },
     });
 
@@ -44,7 +35,7 @@ export async function getStaticProps () {
       },
     };
   } catch (error) {
-    console.error ('Error fetching data:', error);
+    console.error("Error fetching data:", error);
   }
 }
 export default OurClients;
